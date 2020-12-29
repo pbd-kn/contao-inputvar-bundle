b398fb4ad4f841cf71f01cfea7f66bf98a89cba6
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2020-12-29 19:16:29 +0100

    Plugin
    Namespace Managerplugin

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,13 +10,13 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace PBD-KN\ContaoInputVarBundle\ContaoInputVarBundle;
+namespace PBD-KN\\ContaoInputVarBundle\\ContaoManager\\Plugin;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 //use Contao\SkeletonBundle\ContaoSkeletonBundle;
-use PBD-KN\ContaoInputVarBundle\ContaoInputVarBundle;
+use PBD-KN\\ContaoInputVarBundle\\ContaoManager\\Plugin;
 
 class Plugin implements BundlePluginInterface
 {
