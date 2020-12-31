4d1633b66a803c48a96463036f24169e625d46c9
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2020-12-31 18:15:00 +0100

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 
 namespace PBD-KN\InputVarBundle\ContaoManager;
+
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
