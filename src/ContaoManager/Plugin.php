18a0efec98170e8c715271384a6febb6d977006b
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2020-12-31 17:10:22 +0100

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,12 +10,12 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace PBD-KN\ContaoInputVarBundle\ContaoManager;
+namespace Contao\InputVarBundle\ContaoManager;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use PBD-KN\ContaoInputVarBundle\ContaoInputVarBundle;
+use Contao\InputVarBundle\ContaoManager;
 //use Acme\ContaoHelloWorldBundle\ContaoHelloWorldBundle;
 //            BundleConfig::create(ContaoInputVarBundle::class)
 
@@ -24,9 +24,8 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create('PBD-KN\ContaoInputVarBundle\ContaoInputVarBundle')
-                ->setLoadAfter([ContaoCoreBundle::class]),
-        ];
+            BundleConfig::create(ContaoInputVarBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),        ];
     }
 }
 
