703c2afd7d54025a6a653b9d375b724d85d25ba2
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2021-01-05 15:02:18 +0100

    Update ContaoInputVarExtension.php

--- src/DependencyInjection/ContaoInputVarExtension.php
+++ src/DependencyInjection/ContaoInputVarExtension.php
@@ -16,12 +16,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-
 class ContaoInputVarExtension  extends Extension
 {
     public function load(array $mergedConfig, ContainerBuilder $container): void
